a2bbd1c251eb41e9050e9bbdc9706e6285691811
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-10-28 19:05:02 -0300

    feat: implementado teste para a classe Product req1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto='nome produto',
+        nome_da_empresa='nome empresa',
+        data_de_fabricacao='data fabricação',
+        data_de_validade='data validade',
+        numero_de_serie='000',
+        instrucoes_de_armazenamento='armazenamento',
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == 'nome produto'
+    assert product.nome_da_empresa == 'nome empresa'
+    assert product.data_de_fabricacao == 'data fabricação'
+    assert product.data_de_validade == 'data validade'
+    assert product.numero_de_serie == '000'
+    assert product.instrucoes_de_armazenamento == 'armazenamento'

6a8c0152a4815835bee43428bf99a0b90127152c
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-10-28 19:39:22 -0300

    feat: implementado teste para o retorno da classe Product req-2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto='product',
+        nome_da_empresa='company',
+        data_de_fabricacao='01/01/22',
+        data_de_validade='01/01/25',
+        numero_de_serie='000',
+        instrucoes_de_armazenamento='storage',
+    )
+    assert product.__repr__() == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
